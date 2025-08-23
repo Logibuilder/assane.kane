d90cf177661373aaf16b4b2cc3bab39382bc856c
Logibuilder <161773002+Logibuilder@users.noreply.github.com>
2025-08-24 01:29:14 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repo = "assane.kane"; // Remplace par ton repo
+const repo = "assane.kane";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
   images: {
     unoptimized: true,
   },
-  output: "export", // 🔥 Ajoute cette ligne
+  output: "export",
+  trailingSlash: true,
+  // Important pour GitHub Pages
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isGithubActions ? `/${repo}` : '',
+  },
 };
 
 module.exports = nextConfig;
